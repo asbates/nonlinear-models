6880e7d0cb69754e55d01ef505c3011b0832261b
Andrew Bates <andrewbates73@gmail.com>
2019-02-24 12:47:52 -0800

    initial spline comparison

---
+++ splines/univariate-comparison.R
@@ -0,0 +1,46 @@
+
+# this is a simple example to get a visual comparison spline methods
+# here we look at 'regular', natural, and smoothing splines
+# for a discussion see https://asbates.rbind.io as this is really
+#  just some prep for a blog post
+
+library(tidyverse)
+library(splines)
+
+theme_set(theme_bw())
+turq <- "#00CED1"
+
+# generate some data
+set.seed(42)
+
+x <- rnorm(500)
+y <- 1 + 2*x + 3*x^2 + 4*x^3 + rnorm(500, sd = 5)
+true_y <- 1 + 2*x + 3*x^2 + 4*x^3
+df <- tibble(x, y, true_y)
+
+
+ggplot(df, aes(x, y)) +
+  geom_point() +
+  geom_line(aes(x, true_y), color = turq)
+
+
+# fit 3 types of splines
+b_spline <- lm(y ~ bs(x, df = 6), data = df)
+natural_spline <- lm(y ~ ns(x, df = 6), data = df)
+smooth_spline <- smooth.spline(df$x, df$y)
+
+# append to original data frame
+model_df <- df %>% 
+  bind_cols(b_spline = b_spline$fitted.values,
+            natural_spline = natural_spline$fitted.values,
+            smooth_spline = smooth_spline$y
+            ) %>% 
+  gather(true_y, b_spline, natural_spline, smooth_spline, 
+         key = "model", value = "fit")
+
+
+ggplot(model_df, aes(x, y)) +
+  geom_point() +
+  geom_line(aes(x, fit, color = model))
+
+

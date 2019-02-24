03374b7ca698689e00088af71b1c264edec0d320
Andrew Bates <andrewbates73@gmail.com>
2019-02-24 13:18:28 -0800

    get smooth.spline fitted values

--- splines/univariate-comparison.R
+++ splines/univariate-comparison.R
@@ -12,7 +12,6 @@ turq <- "#00CED1"
 
 # generate some data
 set.seed(42)
-
 x <- rnorm(500)
 y <- 1 + 2*x + 3*x^2 + 4*x^3 + rnorm(500, sd = 5)
 true_y <- 1 + 2*x + 3*x^2 + 4*x^3
@@ -27,20 +26,33 @@ ggplot(df, aes(x, y)) +
 # fit 3 types of splines
 b_spline <- lm(y ~ bs(x, df = 6), data = df)
 natural_spline <- lm(y ~ ns(x, df = 6), data = df)
-smooth_spline <- smooth.spline(df$x, df$y)
+smooth_spline <- smooth.spline(df$x, df$y, cv = TRUE)
+
+
+ggplot(df, aes(x, y)) +
+  geom_point() +
+  geom_line(aes(x, true_y), color = turq) +
+  geom_line(aes(x, b_spline$fitted.values)) +
+  geom_line(aes(x, natural_spline$fitted.values)) +
+  geom_line(aes(x, smooth_spline$y))
+
+
+plot(x, y, pch = 16)
+lines(smooth_spline)
+
+
 
 # append to original data frame
 model_df <- df %>% 
   bind_cols(b_spline = b_spline$fitted.values,
             natural_spline = natural_spline$fitted.values,
-            smooth_spline = smooth_spline$y
-            ) %>% 
+            smooth_spline = fitted.values(smooth_spline)) %>% 
   gather(true_y, b_spline, natural_spline, smooth_spline, 
          key = "model", value = "fit")
 
+model_df
 
 ggplot(model_df, aes(x, y)) +
   geom_point() +
   geom_line(aes(x, fit, color = model))
-
 

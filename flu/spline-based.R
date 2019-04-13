6f28041d31a961349b261292d4303f8ee91bf692
Andrew Bates <andrewbates73@gmail.com>
2019-04-13 11:53:27 -0700

    start Tps and mars for spline based flu models

---
+++ flu/spline-based.R
@@ -0,0 +1,100 @@
+
+# model flu data using spline based methods
+
+
+library(tidyverse)
+library(fields)  # tps
+library(earth)  # MARS
+library(mgcv)  # GAM
+
+theme_set(theme_bw())
+
+flu <- read_csv("data/ilinet-calif-up-to-2019-03-31.csv")
+
+flu
+
+flu %>% 
+  ggplot(aes(week_start, unweighted_ili)) +
+  geom_line()
+
+flu %>% 
+  ggplot(aes(week_start, num_of_providers)) +
+  geom_line()
+
+flu %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line()
+
+# use one month of previous data as predictors
+flu <- flu %>% 
+  mutate(
+    lag_ilitotal = lag(ilitotal),
+    lag2_ilitotal = lag(ilitotal, n = 2L),
+    lag3_ilitotal = lag(ilitotal, n = 3L),
+    lag4_ilitotal = lag(ilitotal, n = 4L)
+  )
+
+
+# this assigns 13 weeks or 3 months to testing
+# Tps doesn't handle NAs
+flu_train <- filter(flu, week_start < "2019-01-01") %>% 
+  filter(!is.na(lag4_ilitotal))
+  
+flu_test <- filter(flu, week_start >= "2019-01-01")
+
+flu_train %>% 
+  summarise(start = min(week_start), end = max(week_start))
+
+flu_test %>% 
+  summarise(start = min(week_start), end = max(week_start))
+
+flu_train %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line()
+
+x_train <- flu_train %>% 
+  select(starts_with("lag"))
+
+y_train <- flu_train %>% 
+  select(ilitotal)
+
+flu_tps <- Tps(x_train, y_train)
+
+plot(flu_tps)
+
+flu_train %>% 
+  mutate(.pred = predict(flu_tps)[,1]) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = .pred), color = "grey")
+
+flu_train %>% 
+  mutate(.pred = predict(flu_tps)[,1]) %>% 
+  yardstick::metrics(truth = ilitotal, estimate = .pred)
+
+
+flu_mars <- earth(
+  ilitotal ~ lag_ilitotal + lag2_ilitotal + lag3_ilitotal + lag4_ilitotal,
+  data = flu_train)
+
+flu_train %>% 
+  mutate(.pred = predict(flu_mars)[,1]) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = .pred), color = "grey")
+
+flu_train %>% 
+  mutate(.pred = predict(flu_mars)[,1]) %>% 
+  yardstick::metrics(truth = ilitotal, estimate = .pred)
+
+# shoud we also include lags of number of providers?
+# fewer providers reporting might mean lower number of reported cases
+# it also might be an indicator of data quality/accuracy b/c there might be
+#  unreported cases with fewer providers reporting
+flu <- flu %>% 
+  mutate(
+    lag_num_providers = lag(num_of_providers),
+    lag2_num_providers = lag(num_of_providers, n = 2L),
+    lag3_num_providers = lag(num_of_providers, n = 3L),
+    lag4_num_providers = lag(num_of_providers, n = 4L)
+  )

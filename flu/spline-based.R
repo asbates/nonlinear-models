2f0fd2014d4bd0930ee7b4df98449d3cdc0fe12f
Andrew Bates <andrewbates73@gmail.com>
2019-04-19 14:49:04 -0700

    flu: finish up main part

--- flu/spline-based.R
+++ flu/spline-based.R
@@ -10,7 +10,6 @@ library(fields)  # tps
 library(earth)  # MARS
 library(mgcv)  # GAM
 library(forecast)
-library(yardstick)
 
 theme_set(theme_bw())
 
@@ -52,14 +51,15 @@ flu %>%
 ggAcf(flu_ts)
 ggPacf(flu_ts)
 
-# =========================================
-# ====== models as function of time =======
-# =========================================
 
 # baseline model
-# this took a long time on my computer
 arima_mod <- auto.arima(flu_ts_train)
 arima_mod
+
+
+# =========================================
+# ====== models using time as 1,2,... =====
+# =========================================
 
 # thin plate spline
 tps_mod <- Tps(x_train, y_train)
@@ -89,6 +89,94 @@ flu_train %>%
   geom_line(aes(y = value, color = model))
 
 
+# see how top models do on test set
+flu_test <- flu_test %>% 
+  mutate(
+    pred_arima = forecast(arima_mod, h = 13)$mean[1:13],
+    pred_tps = predict(tps_mod, x_test)[,1]
+  )
+
+flu_test %>% 
+  select(week_start, ilitotal, starts_with("pred")) %>% 
+  gather("model", "forecast", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "pred_")) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = forecast, color = model))
+
+
+flu %>%
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(week_start, pred_arima), data = flu_test, color = "blue") +
+  geom_line(aes(week_start, pred_tps), data = flu_test, color = "red")
+
+
+# ============================================
+# ===== models using time as year, week ======
+# ============================================
+flu_train <- filter(flu, week_start < "2019-01-01")
+flu_test <- filter(flu, week_start >= "2019-01-01")
+
+x_train <- flu_train %>%
+  select(year, week)
+
+y_train <- flu_train %>% 
+  select(ilitotal)
+
+x_test <- flu_test %>% 
+  select(year, week)
+
+y_test <- flu_test %>% 
+  select(ilitotal)
+
+tps_mod <- Tps(x_train, y_train)
+mars_mod <- earth(ilitotal ~ year + week, data = flu_train)
+gam_mod <- gam(ilitotal ~ s(year) + s(week), data = flu_train, method = "REML")
+
+
+flu_train <- flu_train %>% 
+  mutate(
+    fitted_arima = fitted(arima_mod),
+    fitted_tps = fitted(tps_mod)[,1],
+    fitted_mars = predict(mars_mod)[,1],
+    fitted_gam = fitted(gam_mod)
+  )
+
+flu_train %>% 
+  select(week_start, ilitotal, starts_with("fitted")) %>% 
+  gather("model", "value", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "fitted_")) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = value, color = model))
+
+
+# see how top models do on test set
+flu_test <- flu_test %>% 
+  mutate(
+    pred_tps = predict(tps_mod, x_test)[,1],
+    pred_mars = predict(mars_mod, x_test)[,1],
+    pred_gam = predict(gam_mod, newdata = flu_test)
+  )
+
+flu_test %>% 
+  select(week_start, ilitotal, starts_with("pred")) %>% 
+  gather("model", "forecast", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "pred_")) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = forecast, color = model))
+
+
+flu %>%
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(week_start, pred_tps), data = flu_test, color = "blue") +
+  geom_line(aes(week_start, pred_gam), data = flu_test, color = "red") +
+  geom_line(aes(week_start, pred_mars), data = flu_test, color = "green")
+
+
 
 
 # =================================
@@ -104,8 +192,6 @@ flu <- flu %>%
     lag4_ilitotal = lag(ilitotal, n = 4L)
   )
 
-
-# this assigns 13 weeks or 3 months to testing
 # Tps doesn't handle NAs
 flu_train <- filter(flu, week_start < "2019-01-01") %>% 
   filter(!is.na(lag4_ilitotal))
@@ -113,14 +199,7 @@ flu_train <- filter(flu, week_start < "2019-01-01") %>%
 flu_test <- filter(flu, week_start >= "2019-01-01")
 
 flu_train %>% 
-  summarise(start = min(week_start), end = max(week_start))
-
-flu_test %>% 
-  summarise(start = min(week_start), end = max(week_start))
-
-flu_train %>% 
-  ggplot(aes(week_start, ilitotal)) +
-  geom_line()
+  summarise(start = min(week_start))
 
 x_train <- flu_train %>% 
   select(starts_with("lag"))
@@ -134,62 +213,83 @@ x_test <- flu_test %>%
 y_test <- flu_test %>% 
   select(ilitotal)
 
-flu_tps <- Tps(x_train, y_train)
+tps_mod <- Tps(x_train, y_train)
 
-plot(flu_tps)
-
-flu_train <- flu_train %>% 
-  mutate(.pred_tps = predict(flu_tps)[,1])
-
-flu_train %>% 
-  ggplot(aes(week_start, ilitotal)) +
-  geom_line() +
-  geom_line(aes(y = .pred_tps), color = "grey")
-
-flu_train %>% 
-  yardstick::metrics(truth = ilitotal, estimate = .pred_tps)
-
-
-flu_mars <- earth(
+mars_mod <- earth(
   ilitotal ~ lag_ilitotal + lag2_ilitotal + lag3_ilitotal + lag4_ilitotal,
   data = flu_train)
 
-flu_train <- flu_train %>% 
-  mutate(.pred_mars = predict(flu_mars)[,1])
-
-flu_train %>% 
-  ggplot(aes(week_start, ilitotal)) +
-  geom_line() +
-  geom_line(aes(y = .pred_mars), color = "grey")
-
-flu_train %>% 
-  yardstick::metrics(truth = ilitotal, estimate = .pred_mars)
-
-
-flu_gam <- gam(
+gam_mod <- gam(
   ilitotal ~ s(lag_ilitotal) + s(lag2_ilitotal) + 
-             s(lag2_ilitotal) + s(lag4_ilitotal),
+    s(lag2_ilitotal) + s(lag4_ilitotal),
   data = flu_train,
   method = "REML")
 
+
 flu_train <- flu_train %>% 
-  mutate(.pred_gam = predict(flu_gam))
+  mutate(
+    fitted_tps = fitted(tps_mod)[,1],
+    fitted_mars = predict(mars_mod)[,1],
+    fitted_gam = fitted(gam_mod)
+  )
 
 flu_train %>% 
+  select(week_start, ilitotal, starts_with("fitted")) %>% 
+  gather("model", "value", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "fitted_")) %>% 
   ggplot(aes(week_start, ilitotal)) +
   geom_line() +
-  geom_line(aes(y = .pred_gam), color = "grey")
+  geom_line(aes(y = value, color = model))
 
 
-flu_train %>% 
-  yardstick::metrics(truth = ilitotal, estimate = .pred_gam)
-
+# see how top models do on test set
+flu_test <- flu_test %>% 
+  mutate(
+    pred_tps = predict(tps_mod, x_test)[,1],
+    pred_mars = predict(mars_mod, x_test)[,1],
+    pred_gam = predict(gam_mod, newdata = flu_test)
+  )
 
 flu_test %>% 
-  mutate(.pred_tps = predict(flu_tps,x = x_test)[,1]) %>% 
+  select(week_start, ilitotal, starts_with("pred")) %>% 
+  gather("model", "forecast", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "pred_")) %>% 
   ggplot(aes(week_start, ilitotal)) +
   geom_line() +
-  geom_line(aes(y = .pred_tps), color = "grey")
+  geom_line(aes(y = forecast, color = model))
+
+
+flu %>%
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(week_start, pred_tps), data = flu_test, color = "blue") +
+  geom_line(aes(week_start, pred_gam), data = flu_test, color = "red") +
+  geom_line(aes(week_start, pred_mars), data = flu_test, color = "green")
+
+# mars and gam pretty close so compare numerically
+yardstick::metrics(flu_test, truth = ilitotal, estimate = pred_gam)
+yardstick::metrics(flu_test, truth = ilitotal, estimate = pred_mars)
+
+# and the winner is.... MARS!
+
+
+
+# further investigation into GAM
+
+gam2 <- gam(ilitotal ~ s(t, bs = "gp"), data = flu_train)
+gam2 <- gam(ilitotal ~ s(year, bs = "gp") + s(week, bs = "gp"),
+            data = flu_train)
+gam2 <- gam(ilitotal ~ s(t, bs = "cc"),
+            data = flu_train)
+gam2 <- gam(ilitotal ~ s(t, bs = "cr"),
+            data = flu_train)
+gam2 <- gam(ilitotal ~ s(t, bs = "ad"),
+            data = flu_train)
+
+flu_train %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = fitted(gam2)), color = 'grey')
 
 # shoud we also include lags of number of providers?
 # fewer providers reporting might mean lower number of reported cases

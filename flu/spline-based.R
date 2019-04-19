0de239df7657406a1132147cfb66f24b7d87b584
Andrew Bates <andrewbates73@gmail.com>
2019-04-19 13:34:39 -0700

    flu: models based on time

--- flu/spline-based.R
+++ flu/spline-based.R
@@ -1,29 +1,99 @@
 
 # model flu data using spline based methods
 
+# ========================
+# ======= setup ==========
+# ========================
 
 library(tidyverse)
 library(fields)  # tps
 library(earth)  # MARS
 library(mgcv)  # GAM
+library(forecast)
+library(yardstick)
 
 theme_set(theme_bw())
 
-flu <- read_csv("data/ilinet-calif-up-to-2019-03-31.csv")
+flu <- read_csv("data/ilinet-calif-up-to-2019-03-31.csv") %>% 
+  mutate(t = 1:nrow(.))
 
 flu
 
-flu %>% 
-  ggplot(aes(week_start, unweighted_ili)) +
-  geom_line()
+flu_train <- filter(flu, week_start < "2019-01-01")
+flu_test <- filter(flu, week_start >= "2019-01-01")
 
-flu %>% 
-  ggplot(aes(week_start, num_of_providers)) +
-  geom_line()
+flu_train %>% 
+  summarise(start = min(week_start), end = max(week_start))
+
+flu_test %>% 
+  summarise(start = min(week_start), end = max(week_start))
+
+flu_ts <- ts(flu$ilitotal, frequency = 52)
+flu_ts_train <- flu_ts[1:nrow(flu_train)]
+flu_ts_test <- flu_ts[(nrow(flu_train) + 1):nrow(flu)]
+
+# Tps requires x/y
+x_train <- flu_train %>%
+  select(t)
+
+y_train <- flu_train %>% 
+  select(ilitotal)
+
+x_test <- flu_test %>% 
+  select(t)
+
+y_test <- flu_test %>% 
+  select(ilitotal)
 
 flu %>% 
   ggplot(aes(week_start, ilitotal)) +
   geom_line()
+
+ggAcf(flu_ts)
+ggPacf(flu_ts)
+
+# =========================================
+# ====== models as function of time =======
+# =========================================
+
+# baseline model
+# this took a long time on my computer
+arima_mod <- auto.arima(flu_ts_train)
+arima_mod
+
+# thin plate spline
+tps_mod <- Tps(x_train, y_train)
+
+# MARS
+mars_mod <- earth(ilitotal ~ t, data = flu_train)
+
+# GAM
+gam_mod <- gam(ilitotal ~ s(t), data = flu_train, method = "REML")
+
+
+flu_train <- flu_train %>% 
+  mutate(
+    fitted_arima = fitted(arima_mod),
+    fitted_tps = fitted(tps_mod)[,1],
+    fitted_mars = predict(mars_mod)[,1],
+    fitted_gam = fitted(gam_mod)
+  )
+
+
+flu_train %>% 
+  select(week_start, ilitotal, starts_with("fitted")) %>% 
+  gather("model", "value", -week_start, -ilitotal) %>% 
+  mutate(model = str_remove(model, "fitted_")) %>% 
+  ggplot(aes(week_start, ilitotal)) +
+  geom_line() +
+  geom_line(aes(y = value, color = model))
+
+
+
+
+# =================================
+# ======= models using lags =======
+# =================================
 
 # use one month of previous data as predictors
 flu <- flu %>% 

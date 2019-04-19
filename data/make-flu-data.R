30e100b04c8aa72d30edabd90d57adfc7304938f
Andrew Bates <andrewbates73@gmail.com>
2019-04-19 11:11:21 -0700

    flu: add year and week to data

--- data/make-flu-data.R
+++ data/make-flu-data.R
@@ -6,8 +6,10 @@ library(tidyverse)
 ili <- ilinet("state")
 
 calif <- ili %>% 
-  filter(region == "California") %>% 
+  filter(region == "California", week_start < "2019-04-01") %>% 
   select(week_start,
+         year,
+         week,
          unweighted_ili,
          ilitotal,
          num_of_providers,
